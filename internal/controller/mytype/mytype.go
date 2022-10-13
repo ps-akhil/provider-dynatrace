dbb5456cfdc5a4609227e69c43b1497b2d5eea6a
“ps-akhil” <akhilsathyan44@gmail.com>
2022-10-13 12:14:32 +0000

    provider name replace

--- internal/controller/mytype/mytype.go
+++ internal/controller/mytype/mytype.go
@@ -32,9 +32,9 @@ import (
 	"github.com/crossplane/crossplane-runtime/pkg/reconciler/managed"
 	"github.com/crossplane/crossplane-runtime/pkg/resource"
 
-	"github.com/crossplane/provider-template/apis/sample/v1alpha1"
-	apisv1alpha1 "github.com/crossplane/provider-template/apis/v1alpha1"
-	"github.com/crossplane/provider-template/internal/controller/features"
+	"github.com/crossplane/provider-dynatrace/apis/sample/v1alpha1"
+	apisv1alpha1 "github.com/crossplane/provider-dynatrace/apis/v1alpha1"
+	"github.com/crossplane/provider-dynatrace/internal/controller/features"
 )
 
 const (
